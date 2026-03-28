4291f441fc0281db366f5a5aa33ed391dd78dbb8
ka-tan <turns.forms.0g@icloud.com>
2026-03-28 23:23:35 +0800

    dup env

--- minishell_utils.c
+++ minishell_utils.c
@@ -6,20 +6,68 @@
 /*   By: ka-tan <ka-tan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 12:30:09 by zchoo             #+#    #+#             */
-/*   Updated: 2026/03/28 21:04:25 by ka-tan           ###   ########.fr       */
+/*   Updated: 2026/03/28 22:59:20 by ka-tan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static int env_count(char **env)
+{
+	int	i;
+	while (env && env[i])
+		i++;
+	return (i);
+}
+
+static char	**dup_env(char **env)
+{
+	int		i;
+	int 	n;
+	char	**copy;
+	
+	n = env_count(env);
+	copy = malloc(sizeof(char *) * (n + 1));
+	if (!copy)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		copy[i] = ft_strdup(env[i]);
+		if(!copy[i])
+		{
+			while (i >= 0)
+			{
+				i--;
+				free(copy[i]);
+			}
+			free(copy);
+			return (NULL);
+		}
+		i++;
+	}
+	copy[n] = NULL;
+	return (copy);
+}
+
 void init_shell(t_shell *shell, char **env)
 {
 	shell->status = 0;
-	shell->env = env;
+	shell->env = dup_env(env);
+	if (!shell->env)
+		exit(EXIT_FAILURE);
 }
 
 void free_shell(t_shell *shell)
 {
+	int	i;
+
+	i = 0;
+	while (shell->env && shell->env[i])
+	{
+		free(shell->env[i]);
+		i++;
+	}
 	free(shell->env);
 }
 
