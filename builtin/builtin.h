8e27c2caff1554af560a1b770b88d05a80d2fa15
ka-tan <turns.forms.0g@icloud.com>
2026-04-05 19:32:03 +0800

    - added cd return to home - update signature to include t_shell parameter

--- builtin/builtin.h
+++ builtin/builtin.h
@@ -6,7 +6,7 @@
 /*   By: ka-tan <ka-tan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/02 12:35:04 by zchoo             #+#    #+#             */
-/*   Updated: 2026/04/04 23:12:53 by ka-tan           ###   ########.fr       */
+/*   Updated: 2026/04/05 16:41:42 by ka-tan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	builtin_pwd(t_shell *shell);
 int	builtin_env(t_shell	*shell, char **argv);
 int	builtin_export(t_shell *shell, char **argv);
 int	builtin_unset(t_shell *shell, char **argv);
-int	builtin_cd(char **argv);
+int	builtin_cd(t_shell *shell, char **argv);
 int	builtin_exit(t_shell *shell, char **argv);
 int	ft_atoll_exit(char *str, long long *out);
 
--- builtin/builtin_cd.c
+++ builtin/builtin_cd.c
@@ -3,33 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_cd.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zchoo <zchoo@student.42singapore.sg>       +#+  +:+       +#+        */
+/*   By: ka-tan <ka-tan@student.42singapore.sg>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/23 16:06:37 by ka-tan            #+#    #+#             */
-/*   Updated: 2026/04/04 16:18:03 by zchoo            ###   ########.fr       */
+/*   Updated: 2026/04/05 19:30:16 by ka-tan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
-int	builtin_cd(char **argv)
+int	builtin_cd(t_shell *shell, char **argv)
 {
-	if (!argv[1])
-	{
-		ft_putstr_fd("minishell: cd ", 2);
-		ft_putendl_fd(": missing argument", 2);
-		return (1);
-	}
+	char	*path;
+
 	if (argv[2])
 	{
-		ft_putstr_fd("minishell: cd ", 2);
+		ft_putstr_fd("minishell: cd", 2);
 		ft_putendl_fd(": too many arguments", 2);
 		return (1);
 	}
-	if (chdir(argv[1]) == -1)
+	if (!argv[1])
 	{
-		perror("minishell: cd");
-		return (1);
+		path = get_env_value(shell->env, "HOME");
+		if (!path)
+		{
+			ft_putstr_fd("minishell: cd", 2);
+			ft_putendl_fd(": HOME not set", 2);
+			return (1);
+		}
 	}
+	else
+		path = argv[1];
+	if ((chdir(path)) == -1)
+		return (perror("minishell: cd"), 1);
 	return (0);
 }
